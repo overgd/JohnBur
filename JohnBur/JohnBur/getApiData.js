7f72ecdf446e79148d2af4b9c4b9eb5e5c77dc67
overgd <overgd22@naver.com>
2017-07-03 13:57:36 +0900

    init

---
+++ JohnBur/JohnBur/getApiData.js
@@ -0,0 +1,11 @@
+﻿const path = require('path');
+const request = net.request();
+
+function setRequestConfig(_method, _hostname, _path) {
+    request = net.request({
+        method: _method,
+        protocol: 'https:',
+        hostname: _hostname,
+        path: _path
+    });
+}
